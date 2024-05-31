e50231d4c04f52317ac191ec8a7cf558381bce6e
TapasyaTembhurney <tapasyatembhurney@gmail.com>
2024-05-31 08:42:50 +0530

    Done Hero Section & Logo Changed in Navbar Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,14 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagramSquare } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
+import { TbHexagonLetterTFilled } from "react-icons/tb";
 const Navbar = () => {
   return (
   <nav className='mb-20 flex items-center justify-between py-6'>
         <div className='flex flex-shrink-0 items-center'>
-            <img className='mx-2 w-10' src={logo} alt="logo" />
+       
+       <div className='text-5xl'><TbHexagonLetterTFilled /></div> 
+            {/* <img className='mx-2 w-10' src={logo} alt="logo" /> */}
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
         <FaLinkedin />
