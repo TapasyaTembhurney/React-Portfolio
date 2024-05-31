f6e529994017d7ad77eaccbedd40d79e425e03c4
TapasyaTembhurney <tapasyatembhurney@gmail.com>
2024-05-31 06:24:55 +0530

    Done Navbar Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import logo from "../assets/kevinRushLogo.png"
-// import { FaLinkedin } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 const Navbar = () => {
   return (
-  <nav className='bg-red-300 mb-20 flex items-center justify-between py-6'>
+  <nav className='mb-20 flex items-center justify-between py-6'>
         <div className='flex flex-shrink-0 items-center'>
-            <img src={logo} alt="logo" />
+            <img className='mx-2 w-10' src={logo} alt="logo" />
         </div>
-        <div>
-        {/* <FaLinkedin /> */}
+        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagramSquare />
+        <FaSquareXTwitter />
         </div>
 
   </nav>
