66fc46e5dca0ee7086df7f49867b8b9007300ed7
TapasyaTembhurney <tapasyatembhurney@gmail.com>
2024-06-01 11:22:07 +0530

    Appiled Framer-motion

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Experience from './components/Experience'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 
+
+
 const App = () => {
   return (
     <div className='overflow-x-hidden text-neutral-300 antialiased
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,36 +5,85 @@ import { SiMongodb } from "react-icons/si";
 import { SiRedis } from "react-icons/si";
 import { RiNodejsLine } from "react-icons/ri";
 import { BiLogoPostgresql } from "react-icons/bi";
+
+import { animate, motion } from "framer-motion"
+
+const iconVariants =(duration) =>({
+  initial:{y:-10},
+  animate :{
+    y:[10, -10],
+    transition:{
+      duration:duration,
+      ease:"linear",
+      repeat:Infinity,
+      repeatType:"reverse",
+    },
+  },
+
+})
+
 const Technologies = () => {
   return (
     <div className='border-b border-neutral-800 pb-24 '>
-      <h2 className='my-20 text-center text-4xl'>Technologies</h2>
-      <div className='flex flex-wrap  items-center justify-center gap-4'>
+      <motion.h2 
+      whileInView={{opacity:1 , y:0}}
+      initial={{opacity:0 , y:-100}}
+      transition={{duration:1.5}}
+      className='my-20 text-center text-4xl'>Technologies</motion.h2>
+      <motion.div 
+      whileInView={{opacity:1 , y:0}}
+      initial={{opacity:0, y:-100}}
+      transition={{duration:1.5}}
+      className='flex flex-wrap  items-center justify-center gap-4'>
 
-        <div className='rounded-2xl  border-4 border-neutral-800 p-4'>
-        <RiReactjsLine className='text-6xl text-cyan-400' />
-        </div>
+        <motion.div
+        variants={iconVariants(2.5)}
+        initial="initial"
+        animate="animate"
+        className='rounded-2xl  border-4 border-neutral-800 p-4'>
+        <RiReactjsLine className='text-7xl text-cyan-400' />
+        </motion.div>
 
-        <div className='rounded-2xl  border-4 border-neutral-800 p-4'>
-        <TbBrandNextjs  className='text-6xl text-amber-300'/>
-        </div>
+        <motion.div 
+         variants={iconVariants(3.0)}
+         initial="initial"
+         animate="animate"
+        className='rounded-2xl  border-4 border-neutral-800 p-4'>
+        <TbBrandNextjs  className='text-7xl text-amber-300'/>
+        </motion.div>
 
-        <div className='rounded-2xl  border-4 border-neutral-800 p-4'>
-        <SiMongodb className='text-6xl text-green-500' />
-        </div>
+        <motion.div 
+         variants={iconVariants(2)}
+         initial="initial"
+         animate="animate"
+        className='rounded-2xl  border-4 border-neutral-800 p-4'>
+        <SiMongodb className='text-7xl text-green-500' />
+        </motion.div>
 
-        <div className='rounded-2xl  border-4 border-neutral-800 p-4'>
-        <SiRedis className='text-6xl text-red-600' />
-        </div>
+        <motion.div 
+         variants={iconVariants(4)}
+         initial="initial"
+         animate="animate"
+         className='rounded-2xl  border-4 border-neutral-800 p-4'>
+        <SiRedis className='text-7xl text-red-600' />
+        </motion.div>
 
-        <div className='rounded-2xl  border-4 border-neutral-800 p-4'>
-        <RiNodejsLine className='text-6xl bg-gradient-to-r from-orange-500 via-amber-400 to-slate-800' />
-        </div>
+        <motion.div
+         variants={iconVariants(3)}
+         initial="initial"
+         animate="animate"
+        className='rounded-2xl  border-4 border-neutral-800 p-4'>
+        <RiNodejsLine className='text-7xl bg-gradient-to-r from-orange-500 via-amber-400 to-slate-800' />
+        </motion.div>
 
-        <div className='rounded-2xl  border-4 border-neutral-800 p-4'>
-        <BiLogoPostgresql className='text-6xl bg-gradient-to-r from-lime-400 via-slate-700' />
-        </div>
-      </div>
+        <motion.div 
+         variants={iconVariants(4)}
+         initial="initial"
+         animate="animate"
+        className='rounded-2xl  border-4 border-neutral-800 p-4'>
+        <BiLogoPostgresql className='text-7xl bg-gradient-to-r from-lime-400 via-slate-700' />
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
